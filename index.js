40fec10e6310ae2daa3bc85ca5dc4b371d85fa11
Ilche <ilcheivanovski@gmail.com>
2017-12-29 13:47:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     
     response.end("Hello World Ilche Ivanovski");
-
 });
 
 var port = process.env.PORT || 1337;
